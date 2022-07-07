087cb9b4496eae6f189eef8575629fd89a015244
Jorge Garit <jorgegarit@gmail.com>
2022-07-07 08:03:00 -0400

    completed base script.js with commands

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 // Dom Elements
 var quizTimeEl = document.querySelector("#quizTime");
 var questionsEl = document.querySelector("#questions");
-var answersEl = document.querySelector("#answers");
+var choicesEl = document.querySelector("#choices");
 var startQuizBtn = document.querySelector("#startQuiz");
 var submitBtn = document.querySelector("#submit");
 var initialsEl = document.querySelector("#initials");
@@ -23,7 +23,7 @@ function startCodeQuiz () {
     questionsEl.removeAttribute("class");
 
     // Timer will start counting down
-    timerId = setInterval(clockTick, 1000);
+    timerId = setInterval(timerValue, 1000);
 
     quizTimeEl.textContent = time;
 
@@ -38,6 +38,106 @@ function pullQuestions() {
     var questionTitleEl = document.getElementById("questionTitle");
     questionTitleEl.textContent = ongoingQuestion.title;
 
-    // remove any of the old answers
-    answersEl.innerHTML = "";
+    // remove any of the old chocies available 
+    choicesEl.innerHTML = "";
+
+    // chnaged the choices for the new questions
+    ongoingQuestion.choices.forEach(function(choice, i) {
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", choice);
+
+        choiceNode.textContent = i + 1 + ". " + choice;
+        choiceNode.onclick= choiceSelection;
+
+        // display new  choices on html page
+        choicesEl.appendChild(choiceNode);
+    });
 }
+
+function choiceSelection() {
+    // first check for wrong answer
+    if (this.value !== questions[ongoingQuestionArray].answer) {
+        time= time - 10;
+
+        if (time < 0) {
+            time = 0;
+        }
+
+        // show new time on page 
+        quizTimeEl.textContent = time;
+        answerResultEl.textContent= "Incorrect";
+        answerResultEl.style.color = "#BC4123";
+        answerResultEl.style.fontSize = "15px";  
+    } else {
+        answerResultEl.textContent= "Correct";
+        answerResultEl.style.color = "#BC4123";
+        answerResultEl.style.fontSize = "15px"; 
+    }
+
+    // quickly show answer result if right or wrong
+    answerResultEl.setAttribute("class", "answerResult");
+    setTimeout(function () {
+        answerResultEl.setAttribute("class", "answerResult hidden");
+    }, 1000);
+
+    // next question appears
+    ongoingQuestionArray++;
+
+    if (ongoingQuestionArray === questions.length) {
+        endQuiz();
+    } else {
+        pullQuestions();
+    }
+}
+
+function endQuiz() {
+    clearInterval(timerId); // this will stop the timer
+
+    // Quiz COmplete screen will appear 
+    var quizCompleteEl = document.getElementById("quizComplete");
+    quizCompleteEl.removeAttribute("class");
+
+    // score will appear
+    var scoreEl = document.getElementById("score");
+    scoreEl.textContent = time;
+
+    // move the questionsEl back to hidden
+    questionsEl.setAttribute("class", "hidden");
+}
+
+function timerValue() {
+    time--;
+    quizTimeEl.textContent = time;
+
+    if (time <= 0) {
+        endQuiz();
+    }
+}
+
+function saveScore() {
+    // svae initials
+    var initials = initialsEl.value.trim();
+
+    if (initials !== "") {
+        var scores = JSON.parse(window.localStorage.getItem("scores")) || []; // will pull empty array if their are no scores in local storage
+
+        // if new score
+        var newScore = {
+            score: time,
+            initials : initials
+        };
+
+        // save score to localStorage
+        scores.push(newScore);
+        window.localStorage.setItem("scores", JSON.stringify(scores));
+
+        // take to the highscores.html page
+        window.location.href = "./highscores.html";
+    }
+}
+
+// Start Code Quiz
+startQuizBtn.onclick = startCodeQuiz;
+
+submitBtn.onclick = saveScore;
